5af653d0b990498cc2c49a60d1cd62e11e1e9643
salvador <salvadorjnt@gmail.com>
2018-11-28 00:50:56 -0600

    vistas de contactos de instituciones

--- fundacion/directorio/urls.py
+++ fundacion/directorio/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from directorio.views import AcudeInstitucionAdd, AcudeInstitucionAjaxList, AcudeInstitucionEdit
+from directorio.views import AcudeInstitucionAdd, AcudeInstitucionAjaxList, AcudeInstitucionEdit, \
+    ContactoInstitucionAdd, ContactoInstitucionAjaxList, ContactoInstitucionEdit
 from . import views
 
 app_name = 'directorio'
@@ -12,4 +13,10 @@ urlpatterns = [
     path('acude_institucion/ajax/list/', AcudeInstitucionAjaxList.as_view(), name='list_ajax_acude_institucion'),
     path('acude_institucion/edit/<int:pk>', AcudeInstitucionEdit.as_view(), name='edit_acude_institucion'),
     path('acude_institucion/list/delete/<int:pk>', views.delete_acude_institucion, name='delete_acude_institucion'),
+
+    path('contacto_institucion/add/<int:institucion>', ContactoInstitucionAdd.as_view(), name='add_contacto_institucion'),
+    path('contacto_institucion/list/<int:institucion>', views.list_contacto_institucion, name='list_contacto_institucion'),
+    path('contacto_institucion/ajax/list/<int:institucion>', ContactoInstitucionAjaxList.as_view(), name='list_ajax_contacto_institucion'),
+    path('contacto_institucion/edit/<int:pk>/<int:institucion>', ContactoInstitucionEdit.as_view(), name='edit_contacto_institucion'),
+    path('contacto_institucion/list/delete/<int:pk>', views.delete_contacto_institucion, name='delete_contacto_institucion'),
 ]
--- fundacion/directorio/views.py
+++ fundacion/directorio/views.py
@@ -9,8 +9,8 @@ from django.urls import reverse
 from django.views.generic import CreateView, UpdateView
 from django_datatables_view.base_datatable_view import BaseDatatableView
 
-from adminstrador.forms import AcudeInstitucionForm
-from config.models import AcudeInstitucion
+from adminstrador.forms import AcudeInstitucionForm, ContactoInstitucionForm
+from config.models import AcudeInstitucion, ContactoInstitucion
 
 
 class AcudeInstitucionAdd(PermissionRequiredMixin, CreateView):
@@ -148,4 +148,123 @@ class AcudeInstitucionEdit(PermissionRequiredMixin, UpdateView):
 def delete_acude_institucion(request, pk):
     acude_institucion = get_object_or_404(AcudeInstitucion, pk=pk)
     acude_institucion.delete()
+    return JsonResponse({'result': 1})
+
+class ContactoInstitucionAdd(PermissionRequiredMixin, CreateView):
+    redirect_field_name = 'next'
+    login_url = '/'
+    permission_required = 'directorio'
+
+    model = ContactoInstitucion
+    template_name = 'config/formulario_1Col.html'
+    success_url = '/directorio/contacto_institucion/list'
+    form_class = ContactoInstitucionForm
+
+    def get_context_data(self, **kwargs):
+        context = super(ContactoInstitucionAdd, self).get_context_data(**kwargs)
+        if 'form' not in context:
+            context['form'] = self.form_class()
+        if 'titulo' not in context:
+            context['titulo'] = 'Agregar un contacto_institucion'
+        if 'instrucciones' not in context:
+            context['instrucciones'] = 'Completa todos los campos para registrar un'
+        if 'save_and_new' not in context:
+            context['save_and_new'] = 'Guardar y nuevo'
+
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object
+        form = self.form_class(request.POST)
+        if form.is_valid():
+
+            institucion = AcudeInstitucion.objects.get(pk=self.kwargs['institucion'])
+            cont_institucion = form.save(commit=False)
+
+            cont_institucion.institucion = institucion
+            cont_institucion.save()
+            if 'save_and_new' in form.data:
+                return HttpResponseRedirect(self.guardar_y_nuevo())
+
+            return HttpResponseRedirect(self.get_success_url())
+        else:
+            return self.render_to_response(self.get_context_data(form=form))
+
+    def guardar_y_nuevo(self):
+        return reverse('directorio:add_contacto_institucion', kwargs={'institucion': self.kwargs['institucion']})
+
+    def get_success_url(self):
+        return reverse('directorio:list_contacto_institucion', kwargs={'institucion': self.kwargs['institucion']})
+
+
+@permission_required(perm='directorio', login_url='/')
+def list_contacto_institucion(request, institucion):
+    template_name = 'directorio/tab_contacto_institucion.html'
+    institucion = AcudeInstitucion.objects.get(pk=institucion)
+    context = {'institucion': institucion}
+    return render(request, template_name, context)
+
+
+class ContactoInstitucionAjaxList(PermissionRequiredMixin, BaseDatatableView):
+    redirect_field_name = 'next'
+    login_url = '/'
+    permission_required = 'directorio'
+
+    model = ContactoInstitucion
+    columns = ['id', 'nombre', 'editar', 'eliminar']
+    order_columns = ['id', 'nombre']
+    max_display_length = 100
+
+    def render_column(self, row, column):
+
+        if column == 'editar':
+            return '<a class="" href ="' + reverse('directorio:edit_contacto_institucion',
+                                                   kwargs={
+                                                       'pk': row.pk, 'institucion': self.kwargs[
+                                                           'institucion']}) + '"><img  src="http://orientacionjuvenil.colorsandberries.com/Imagenes/fundacion_origen/3/editar.png"></a>'
+        elif column == 'eliminar':
+            return '<a class=" modal-trigger" href ="#" onclick="actualiza(' + str(
+                row.pk) + ')"><img  src="http://orientacionjuvenil.colorsandberries.com/Imagenes/fundacion_origen/3/eliminar.png"></a>'
+        elif column == 'id':
+            return row.pk
+
+        return super(ContactoInstitucionAjaxList, self).render_column(row, column)
+
+    def get_initial_queryset(self):
+        return ContactoInstitucion.objects.filter(institucion__pk=self.kwargs['institucion'])
+
+    def filter_queryset(self, qs):
+        search = self.request.GET.get(u'search[value]', None)
+        if search:
+            qs = qs.filter(nombre__icontains=search) | qs.filter(pk__icontains=search)
+        return qs
+
+
+class ContactoInstitucionEdit(PermissionRequiredMixin, UpdateView):
+    redirect_field_name = 'next'
+    login_url = '/'
+    permission_required = 'directorio'
+
+    model = ContactoInstitucion
+    template_name = 'config/formulario_1Col.html'
+    form_class = ContactoInstitucionForm
+
+    def get_context_data(self, **kwargs):
+        context = super(ContactoInstitucionEdit, self).get_context_data(**kwargs)
+        if 'form' not in context:
+            context['form'] = self.form_class()
+        if 'titulo' not in context:
+            context['titulo'] = 'Editar '
+        if 'instrucciones' not in context:
+            context['instrucciones'] = 'Modifica o actualiza los datos que requieras'
+        return context
+
+    def get_success_url(self):
+        return reverse('directorio:list_contacto_institucion', kwargs={'institucion': self.kwargs['institucion']})
+
+
+@permission_required(perm='directorio', login_url='/')
+def delete_contacto_institucion(request, pk):
+    contacto_institucion = get_object_or_404(ContactoInstitucion, pk=pk)
+    contacto_institucion.delete()
     return JsonResponse({'result': 1})
