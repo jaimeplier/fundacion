2c966170a6899ca7f145d6a94a41d2228da24c92
salvador <snuno@softicsolutions.mx>
2019-05-06 19:51:54 -0500

    test para seguimiento

---
+++ fundacion/templates/consejero/test_seguimiento.js
@@ -0,0 +1,34 @@
+//var motivo_llamada = $("#tl9").prop('checked', true);
+
+var nivel_riesgo = $('#nivel_riesgo').val(1);
+var f = $('#f').val("Fortalezas de prueba");
+var debilidades = $('#debilidades').val("fortalezas de prueba");
+var amenazas = $('#amenazas').val("amenazas de prueba");
+var recursos = $('#recursos').val("Recursos de prueba");
+var intervencion = $('#intervencion').val("Intervencion de prueba");
+var estado_mental_ute = $('#em_ute').val(1);
+var estado_mental_p = $('#em_p').val(2);
+var estado_mental_l = $('#em_l').val(3);
+var estado_mental_m = $('#em_m').val(1);
+var estado_mental_a = $('#em_a').val(2);
+var fase_cambio = $('#fase_cambio').val(2);
+var modalidad_violencia = $('#modalidad_violencia').val(1);
+var tipo_violencia = $('#tipos_violencia').val(1);
+var violentometro = $('#violentometro').val(1);
+var victimas = $('#victimas').val(1);
+var agresor = $('#agresor').val(1);
+var medio_conatacto = $('#medio_contacto').val(1);
+var plan_accion = $('#plan_accion').val("plan de accion de prueba");
+
+var como_se_entero = $('#como_se_entero').val(2);
+
+var tarea1 = $('#tarea1').val("Tarea de prueba 1");
+var tarea2 = $('#tarea2').val("Tarea de prueba 2");
+var tarea3 = $('#tarea3').val("Tarea de prueba 3");
+
+tipificacion1 = $('#tipificaciones1').val(1);
+var motivo_llamada = $("#tl9").prop('checked', true);
+
+categoria_tipificacion1 = $("#tic63_1").prop('checked', true);
+
+$('select').material_select();
