f71733c81bc3ac55e013b34ff930935dde978b62
salvador <snuno@softicsolutions.mx>
2019-05-30 18:50:53 -0500

    scripts para test

---
+++ fundacion/templates/consejero/test_primeraAbogado.js
@@ -0,0 +1,34 @@
+//var motivo_llamada = $("#tl9").prop('checked', true);
+// datos personales
+
+var nombre_alias = $('#nombre_alias').val("Juanito");
+var sexo = $('#sexo').val(2);
+var cp = $('#cp').val("12345");
+var colonia = $('#colonia').val("colonia de prueba");
+var como_se_entero = $('#como_se_entero').val(1);
+
+var motivo_llamada = $("#tl8").prop('checked', true);
+
+var nivel_riesgo = $('#nivel_riesgo').val(1);
+var f = $('#f').val("Fortalezas de prueba");
+var debilidades = $('#debilidades').val("fortalezas de prueba");
+var amenazas = $('#amenazas').val("amenazas de prueba");
+var recursos = $('#recursos').val("Recursos de prueba");
+var intervencion = $('#intervencion').val("Intervencion de prueba");
+
+var fase_cambio = $('#fase_cambio').val(2);
+var estado_mental_ute = $('#em_ute').val(1);
+var estado_mental_p = $('#em_p').val(2);
+var estado_mental_l = $('#em_l').val(3);
+var estado_mental_m = $('#em_m').val(1);
+var estado_mental_a = $('#em_a').val(2);
+var medio_conatacto = $('#medio_contacto').val(1);
+
+var plan_accion = $('#plan_accion').val("plan de accion de prueba");
+
+
+var tarea1 = $('#tarea1').val("Tarea de prueba 1");
+var tarea2 = $('#tarea2').val("Tarea de prueba 2");
+var tarea3 = $('#tarea3').val("Tarea de prueba 3");
+
+$('select').material_select();
